854c5851a2813c4987c108d41754f55bebae4fd5
Axoford12 <i@aquai.org>
2017-04-23 10:04:23 +0800

    Added version func and some structs

--- daemon/src/server/panel.go
+++ daemon/src/server/panel.go
@@ -11,3 +11,39 @@ Org Rubiginosu
  */
 package server
 
+import(
+    "initial"
+)
+// 验证panel
+func auth(hashCode string) bool {
+    // TODO 实现验证方法
+}
+
+type Amswer struct {
+    Success bool
+    Error string
+    Data string
+}
+
+func getVersion(w http.ResponseWriter,r *http.Request){
+    version := initial.VERSION
+    r.ParseForm()
+    auth := r.Form['auth'][0]
+    var answer answer
+    if auth(auth) {
+        // 验证成功
+        answer.Success = true
+        answer.Error = ""
+        answer.Data = version
+    } else {
+    
+        authInvalid(&answer)
+    }
+    
+}
+
+func authInvalid(answer *Amswer){
+    answer.Success = false
+    answer.Error = "Auth faild"
+    answer.Data = ""
+}
