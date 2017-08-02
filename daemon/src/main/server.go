69a5f0f811789d7998d4077a54efe3fb697d1dcc
Axoford12 <847072154@qq.com>
2017-08-02 13:46:13 +0800

    Write new wrapper

---
+++ daemon/src/main/server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"os/user"
+	"regexp"
+	"syscall"
+)
+
+//#include<unistd.h>
+//#include<malloc.h>
+//void setug(int id){
+//	while(setgid(id)!=0) sleep(1);
+//  while(setuid(id)!=0) sleep(1);
+//}
+import "C"
+
+func main() {
+	var (
+		uid     int
+		mem     int64
+		command string // command: ping www.baidu.com
+		chroot  string // eg : ./server0
+		mounts  string // json mount
+	)
+	flag.IntVar(&uid, "uid", 0, "uid for setuid command")
+	flag.Int64Var(&mem, "mem", 1024, "mem (m) for rlimit")
+	flag.StringVar(&command, "cmd", "", "Command to be run")
+	flag.StringVar(&chroot, "chr", "", "Chroot jail for pro") // 申明并解析参数
+	flag.StringVar(&mounts, "mnts", "", "Mounts dirpath")
+	flag.Parse()
+	usrCur, _ := user.Current()
+	fmt.Printf("User name %s", usrCur.Uid)
+	if usrCur.Uid != "0" {
+		fmt.Printf("Please run as root.")
+		os.Exit(-250)
+	}
+	syscall.Unshare(syscall.CLONE_NEWUTS | syscall.CLONE_NEWIPC | syscall.CLONE_FILES | syscall.CLONE_FS)
+	err := syscall.Setrlimit(syscall.RLIMIT_AS, &syscall.Rlimit{
+		Cur: uint64(mem * 1048576),
+		Max: uint64(mem * 1048576),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	if _, err := os.Stat("/lib64"); err == nil {
+		os.Mkdir(chroot+"/lib64", 0664)
+	}
+	syscall.Chroot(chroot)
+	os.Chdir(chroot + "/serverData")
+	err4 := syscall.Setgroups([]int{uid})
+	if err4 != nil {
+		panic(err4)
+	}
+	// 降权
+	// 以上区域拥有root权限
+	///////////////////////////////////////////////////////////
+	C.setug(C.int(uid))
+
+	commands := regexp.MustCompile(" +").Split(command, -1)
+	err5 := syscall.Exec(commands[0], commands, []string{})
+	if err5 != nil {
+		panic(err5)
+	}
+}
--- daemon/src/main/test.go
+++ daemon/src/main/test.go
@@ -1,32 +1,52 @@
-
 package main
 
+import (
+	"flag"
+	"syscall"
+	"regexp"
+	"os"
+)
+
+//#include<unistd.h>
+//#include<malloc.h>
+//void setug(int id){
+//	while(setgid(id)!=0) sleep(1);
+//  while(setuid(id)!=0) sleep(1);
+//}
+import "C"
+
+
 func main() {
-	//b, _ := json.MarshalIndent(dmserver.ExecInstallConfig{
-	//	[]dmserver.Module{
-	//		{
-	//			Name:     "php7",
-	//			Download: "www.php.net",
-	//			Chmod:    "755,root",
-	//			Md5: "md5",
-	//		},
-	//	},
-	//	true,
-	//	1234,
-	//	"http://www.baidu.com",
-	//	dmserver.ExecConf{
-	//		"pm",
-	//		"php",
-	//		"",
-	//		"",
-	//		"",
-	//		1024,
-	//		1024,
-	//		[]string{"pm.phar"},
-	//		"stop",
-	//	},
-	//	"OK",
-	//	"md5",
-	//}, "", "\t")
-	//fmt.Println(string(b))
+	var (
+		uid     int
+		mem     int64
+		command string // command: ping www.baidu.com
+		chroot string // eg : ./server0
+	)
+	flag.IntVar(&uid, "uid", 0, "uid for setuid command")
+	flag.Int64Var(&mem, "mem", 1024, "mem (m) for rlimit")
+	flag.StringVar(&command, "cmd", "", "Command to be run")
+	flag.StringVar(&chroot,"chr","","Chroot jail for pro")
+	flag.Parse()
+	err := syscall.Setrlimit(syscall.RLIMIT_AS,&syscall.Rlimit{
+		Cur:uint64(mem * 1048576),
+		Max:uint64(mem * 1048576),
+	})
+	if err != nil {
+		panic(err)
+	}
+	syscall.Chroot(chroot)
+	os.Chdir(chroot + "/serverData")
+	err4 := syscall.Setgroups([]int{uid})
+	if err4 != nil {
+		panic(err4)
+	}
+
+	C.setug(C.int(uid))
+
+	commands := regexp.MustCompile(" +").Split(command,-1)
+	err5 := syscall.Exec(commands[0], commands, []string{})
+	if err5 != nil {
+		panic(err5)
+	}
 }
